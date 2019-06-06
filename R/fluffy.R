1bb2454bbb2a65fffa2d42800f65a042bf0883ff
zhewa <zhe@bu.edu>
2019-06-05 22:01:44 -0400

    fluffyChecksum

--- R/fluffy.R
+++ R/fluffy.R
@@ -1,16 +1,24 @@
-#' @title Make fluffy
-#' @description Make stuff fluffy
-#' @param x one or more \R objects, to be converted to character vectors.
-#' @details The fuction surrounds the input characters \code{x} with
-#'  \code{~\{\{} and \code{\{\{~}.
-#' @return A fluffy \code{x}.
-#' @seealso \code{\link{paste}} \code{\link{paste0}}
+#' @title Make fluffy checksum
+#' @description Make the checksum of stuff fluffy
+#' @param x An arbitrary \R object which will then be passed to the
+#'  \code{\link{digest}} function.
+#' @param algo The algorithms to be used; currently available choices are
+#'  \code{md5}, which is also the default, \code{sha1}, \code{crc32},
+#'  \code{sha256}, \code{sha512}, \code{xxhash32}, \code{xxhash64},
+#'  \code{murmur32} and \code{spookyhash}.
+#' @details The fuction calculates the checksum of the input \code{x} and
+#'  surrounds the checksum with \code{~\{\{} and \code{\{\{~}.
+#' @return A fluffy checksum of \code{x}.
+#' @seealso \code{\link{paste}} \code{\link{digest}}
 #' @examples
-#' makeFluffy("a")
-#' makeFluffy(123)
-#' makeFluffy(matrix(rep(1, 9), ncol = 3, nrow = 3))
+#' fluffyChecksum("a")
+#' fluffyChecksum(123, algo = "sha1")
+#' fluffyChecksum(matrix(rep(1, 9), ncol = 3, nrow = 3))
 #' @author Zhe Wang
+#' @importFrom digest digest
 #' @export
-makeFluffy <- function(x) {
-    return(paste0("~{{", x, "}}~"))
+fluffyChecksum <- function(x, algo = "md5") {
+    fluffyChecksum <- digest::digest(x, algo = algo)
+    fluffyChecksum <- paste0("~{{", fluffyChecksum ,"}}~")
+    return(fluffyChecksum)
 }
