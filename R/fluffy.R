c77200f347138ac95901b5ac009291ea35664c76
zhewa <zhe@bu.edu>
2019-06-05 22:37:54 -0400

    excercises

--- R/fluffy.R
+++ R/fluffy.R
@@ -14,3 +14,59 @@
 makeFluffy <- function(x) {
     return(paste0("~{{", x, "}}~"))
 }
+
+
+# Excercises
+# Fill in roxygen2 documentations and complete the codes
+# Don't forget to write the corresponding unit tests!
+# Commit your changes and make a pull request
+
+
+# times two
+# This function returns the input x multiplied by two
+timesTwo <- function(x) {
+    # write your code here
+    res <- NULL
+    return(res)
+}
+
+
+# random number
+# This function returns a random integer between a and b
+randomNumber <- function(a, b) {
+    # write your code here
+    res <- NULL
+    return(res)
+}
+
+
+# factorial
+# This function returns the factorial of x
+# Do not use the factorial built-in R function!
+fact <- function(x) {
+    # write your code here
+    res <- NULL
+    return(res)
+}
+
+
+# Bonus questions:
+
+# factorial (recursion)
+# This function returns the factorial of x using recursion
+# Do not use the factorial built-in R function!
+factRecursion <- function(x) {
+    # write your code here
+    res <- NULL
+    return(res)
+}
+
+
+# factorial (memoization)
+# This function returns the factorial of x using memoization
+# Do not use the factorial built-in R function!
+factMemoiz <- function(x) {
+    # write your code here
+    res <- NULL
+    return(res)
+}
